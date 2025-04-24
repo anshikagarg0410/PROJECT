95ca1f12cad0fe669a2c3009862353e9cb1318c8
anshika <anshika8556@proton.me>
2025-04-25 02:28:31 +0530

    adding sell part

---
+++ app/src/main/java/com/igdtuw/myapplication/youraccount.kt
@@ -0,0 +1,26 @@
+package com.igdtuw.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.igdtuw.myapplication.R
+
+class youraccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youraccount)
+        Log.d("DEBUG_SCREEN", "YourAccount activity launched")
+
+        val backIcon = findViewById<ImageView>(R.id.backIcon)
+        backIcon.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // optional: closes current activity
+        }
+    }
+
+}
